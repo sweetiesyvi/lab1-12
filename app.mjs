d136fe6ba776aa88e0c16ee971e74d794f57aab3
sweetie <rineemwky@gmail.com>
2025-12-01 10:57:55 -0600

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -5,7 +5,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('hello world')
 })
-app.get('/api/classData', (req, res) => {
-    res.sendFile(join())
+app.get('classData', (req, res) => {
+    res.sendFile('choooooooooooooos')
 })
+
 app.listen(3000)
